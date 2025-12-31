46bbf89e7804b30a6f4709b2f06d15e4e637ae22
Alexander040105 <74027875+Alexander040105@users.noreply.github.com>
2025-12-31 18:34:06 +0800

    starting to test out pyqt

---
+++ v2/app.py
@@ -0,0 +1,66 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QTextEdit
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+from vector import retriever
+import speech_recognition as sr
+import pyttsx3
+
+recognizer = sr.Recognizer()
+mic = sr.Microphone()
+
+model = OllamaLLM(model="llama3.2")
+
+template = """
+You are a Terraria Wiki expert.
+Always answer using in-game mechanics only and explain it in a Dr.House way.
+If the information is not in the wiki context, say that you don't know and do not make up an answer.
+
+Wiki context:
+{wiki}
+
+Question:
+{question}
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+
+def speak_ai(text):
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)
+    engine.say(text)
+    engine.runAndWait()
+
+def voice_input():
+    with mic as source:
+        speak_ai("Ask me a question about Terraria!")
+        print("\n🎤 Listening... (say 'q' to quit)")
+        recognizer.adjust_for_ambient_noise(source, duration=0.5)
+        audio = recognizer.listen(source, timeout=15, phrase_time_limit=30)
+
+    try:
+        question = recognizer.recognize_google(audio)
+    except:
+        return
+    print(f"\n🗣️ You asked: {question}")
+
+    docs = retriever.invoke(question)
+    wiki = "\n\n".join(doc.page_content for doc in docs)
+
+    answer = chain.invoke({"wiki": wiki, "question": question})
+    speak_ai(answer)
+    
+app = QApplication(sys.argv)
+window = QMainWindow()
+layout = QVBoxLayout()
+
+voice_button = QPushButton("Start Voice Input")
+voice_button.setCheckable(True)
+voice_button.clicked.connect(voice_input)
+
+window.show()
+app.exec()
+
